9a2fd5140920b143ee7189a1c69b013b730b4c6d
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-04-18 11:04:51 -0300

    criando serviços de validação e de sessão

--- script.php
+++ script.php
@@ -10,43 +10,23 @@ $categorias[] = "adulto";
 $nome = $_POST["nome"];
 $idade = $_POST["idade"];
 
-if(empty($nome)) {
-  $_SESSION["mensagem-de-erro"] =  "O nome não pode ser vazio. Informe um nome.";
+
+if($idade >= 6 && $idade <= 12){
+  $_SESSION["mensagem-de-sucesso"] =  "O atleta $nome compete na categoria $categorias[0]";
   header("Location: index.php");
   return;
-}elseif(strlen($nome) < 3) {
-  $_SESSION["mensagem-de-erro"] =  "O nome não pode conter menos de 3 caracteres.";
+} elseif($idade >= 13 && $idade <= 18){
+  $_SESSION["mensagem-de-sucesso"] =  "O atleta $nome compete na categoria $categorias[1]";
   header("Location: index.php");
   return;
-}elseif(strlen($nome) > 40) {
-  $_SESSION["mensagem-de-erro"] =  "O nome é muito extenso.";
+} elseif($idade >= 19){
+  $_SESSION["mensagem-de-sucesso"] =  "O atleta $nome compete na categoria $categorias[2]";
   header("Location: index.php");
   return;
-}elseif(empty($idade)) {
-  $_SESSION["mensagem-de-erro"] =  "A idade não pode ser vazia.";
+} else {
+  $_SESSION["mensagem-de-sucesso"] =  "O atleta não compete!";
   header("Location: index.php");
   return;
-}elseif(!is_numeric($idade)) {
-  $_SESSION["mensagem-de-erro"] =  "Idade não válida.";
-  header("Location: index.php");
-  return;
-}else{
- if($idade >= 6 && $idade <= 12){
-    $_SESSION["mensagem-de-sucesso"] =  "O atleta $nome compete na categoria $categorias[0]";
-    header("Location: index.php");
-    return;
-  } elseif($idade >= 13 && $idade <= 18){
-    $_SESSION["mensagem-de-sucesso"] =  "O atleta $nome compete na categoria $categorias[1]";
-    header("Location: index.php");
-    return;
-  } elseif($idade >= 19){
-    $_SESSION["mensagem-de-sucesso"] =  "O atleta $nome compete na categoria $categorias[2]";
-    header("Location: index.php");
-    return;
-  } else {
-    $_SESSION["mensagem-de-sucesso"] =  "O atleta não compete!";
-    header("Location: index.php");
-    return;
-  }
 }
+
 
