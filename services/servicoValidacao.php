7373dcafc31861a22289d15e8dea3a00b34bb1d9
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-04-18 11:05:40 -0300

    criado serviços de validação e de sessão

---
+++ services/servicoValidacao.php
@@ -0,0 +1,28 @@
+<?php
+
+function validaNome(string $nome) : bool {
+  if(empty($nome)) {
+    setarMensagemErro("O nome não pode ser vazio. Informe um nome.");
+    return false;
+  }elseif(strlen($nome) < 3) {
+    setarMensagemErro("O nome não pode conter menos de 3 caracteres.");
+    return false;
+  }elseif(strlen($nome) > 40) {
+    setarMensagemErro("O nome é muito extenso.");
+    return false;
+  }
+  return true;
+}
+
+function validaIdade(string $idade) : bool {
+  if(empty($idade)) {
+    setarMensagemErro("A idade não pode ser vazia.");
+    return false;
+  }elseif(!is_numeric($idade)) {
+    setarMensagemErro("Idade não válida.");
+    return false;
+  }
+  return true;
+}
+
+
